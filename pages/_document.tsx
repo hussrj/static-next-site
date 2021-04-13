5cd441b459c7045c6c0ca9c309591454b91435b1
Rob Hussey <hussrj@gmail.com>
2021-04-13 03:14:31 +0100

    Work around flash of unstyled content https://bugzilla.mozilla.org/show_bug.cgi?id=1404468 with empty script tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+          <script>0</script>
           <Main />
           <NextScript />
         </body>
