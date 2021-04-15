a4c93f29370f8ff18a952c5f8ddae2c7f45075f7
Rob Hussey <hussrj@gmail.com>
2021-04-14 23:03:23 -0400

    Optimize og image

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 import Author from '../types/author'
 
-export const HOME_OG_IMAGE_URL = '/ogrjhuss.png'
+export const HOME_OG_IMAGE_URL = '/ogrjhuss.jpg'
 export const SITE_TITLE = 'RJHUSS'
 export const OWNER_AUTHOR: Author = {
     name: "Rob Hussey",
