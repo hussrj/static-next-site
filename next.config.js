ce2f15e3cbd54cad2bedd4f56c2c38056839f05e
Rob Hussey <hussrj@gmail.com>
2023-06-15 18:02:36 -0400

    reconfigure for output standalone

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone',
-    experimental: {
-      outputStandalone: true,
-    }
+    output: 'standalone'
   }
 
 module.exports = nextConfig
