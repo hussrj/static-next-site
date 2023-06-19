804761baeb40a9c6b71171b93fb433133deb396b
Rob Hussey <hussrj@gmail.com>
2023-06-19 12:05:09 -0400

    prefer static for comp list and refactor code dupe

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,10 @@
+import { API, graphqlOperation } from "aws-amplify";
 import fs from 'fs'
 import { join } from 'path'
 import matter from 'gray-matter'
+import { ExternalComponent, ListExternalComponentsQuery } from "src/API";
+import { listExternalComponents } from "src/graphql/queries";
+import { GraphQLResult } from "@aws-amplify/api";
 
 const postsDirectory = join(process.cwd(), '_posts')
 
@@ -44,4 +48,43 @@ export function getAllPosts(fields: string[] = []) {
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
   return posts
+}
+
+export async function getAllComponents(): Promise<ExternalComponent[]> {
+  const externalToolsResult = (await API.graphql(
+    graphqlOperation(listExternalComponents, {
+      filter: {
+        tags: {
+          contains: "rjhuss",
+        },
+      },
+      authMode: "AWS_IAM",
+    })
+  )) as GraphQLResult<ListExternalComponentsQuery>;
+  let externalComponents: ExternalComponent[] = [];
+  if (
+    externalToolsResult.data !== undefined &&
+    externalToolsResult.data.listExternalComponents !== undefined &&
+    externalToolsResult.data.listExternalComponents !== null
+  ) {
+    externalToolsResult.data.listExternalComponents.items?.map((extComp: ExternalComponent | null) => (
+      externalComponents.push(
+        extComp = extComp ? extComp : {
+          __typename: "ExternalComponent",
+          id: "1",
+          name: "Loading...",
+          href: "",
+          tags: [""],
+          _version: 1,
+          _deleted: false,
+          _lastChangedAt: 1,
+          createdAt: "2020-04-10",
+          updatedAt: "2020-04-10",
+        })
+      )
+    );
+  } else {
+    externalComponents = [];
+  }
+  return externalComponents;
 }
