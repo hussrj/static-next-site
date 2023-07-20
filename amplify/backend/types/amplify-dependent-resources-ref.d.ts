0a5dd861f33d5bc1042847d28a95882329a01602
Rob <hussrj@gmail.com>
2023-07-19 21:05:07 -0400

    Dev (#31)
    * prefer static for comp list and refactor code dupe
    
    * Reduce safari pinned tab svg (#28)
    
    * Update amp and doc (#30)
    
    * update amplify backend and nextjs _document
    
    * add provider info to gitignore
    
    * newline formatting

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "RJHussAPI": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "rjhussappauth": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,42 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+    DocumentInitialProps,
+} from 'next/document'
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link href="/fonts/style.css" rel="stylesheet"/>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+class StaticNextSiteDocument extends Document {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+        const originalRenderPage = ctx.renderPage
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => App,
+                enhanceComponent: (Component) => Component,
+            })
+
+        const initialProps = await Document.getInitialProps(ctx)
+
+        return initialProps
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link href="/fonts/style.css" rel="stylesheet"/>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
+
+export default StaticNextSiteDocument
